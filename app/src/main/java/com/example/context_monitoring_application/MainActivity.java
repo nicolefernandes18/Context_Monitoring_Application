85d00c325f8137b2c5080cc6a3151b5610d7b3c7
nicolefernandes18 <37707051+nicolefernandes18@users.noreply.github.com>
2023-09-03 14:03:50 -0700

    Added few features

--- app/src/main/java/com/example/context_monitoring_application/MainActivity.java
+++ app/src/main/java/com/example/context_monitoring_application/MainActivity.java
@@ -1,13 +1,97 @@
 package com.example.context_monitoring_application;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.TotalCaptureResult;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.TextureView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+import android.view.Surface;
+import android.util.Size;
+import android.os.Handler;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import android.util.Log;
+import java.util.*;
+
+import android.content.Context;
+import android.graphics.ImageFormat;
+import android.media.ImageReader;
+import android.os.HandlerThread;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CAMERA_PERMISSION = 101;
+
+    private TextureView cameraPreview;
+    private TextureView.SurfaceTextureListener surfaceTextureListener = new TextureView.SurfaceTextureListener() {
+        @Override
+        public void onSurfaceTextureAvailable(@NonNull SurfaceTexture surfaceTexture, int width, int height) {
+            setupCamera(width, height);
+        }
+
+        @Override
+        public void onSurfaceTextureSizeChanged(@NonNull SurfaceTexture surfaceTexture, int width, int height) {
+
+        }
+
+        @Override
+        public boolean onSurfaceTextureDestroyed(@NonNull SurfaceTexture surfaceTexture) {
+            return false;
+        }
+
+        @Override
+        public void onSurfaceTextureUpdated(@NonNull SurfaceTexture surfaceTexture) {
+
+        }
+    };
+
+    private Button measureHrtRate;
+
+    private CameraDevice cameraDevice;
+    private CameraDevice.StateCallback stateCallback = new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(CameraDevice camera) {
+            cameraDevice = camera;
+        }
+
+        @Override
+        public void onDisconnected(CameraDevice camera) {
+            camera.close();
+            cameraDevice = null;
+        }
+
+        @Override
+        public void onError(CameraDevice camera, int i) {
+            camera.close();
+            cameraDevice = null;
+        }
+    };
+
+    private HandlerThread backgroundHandlerThread;
+    private Handler backgroundHandler;
+    private String cameraIds;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +106,69 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, Symptoms.class));
             }
         });
+
+        cameraPreview = (TextureView) findViewById(R.id.cameraPreview);
+        measureHrtRate = findViewById(R.id.heartRateBtn);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+
+        startBackgroundThread();
+
+        if(cameraPreview.isAvailable()){
+            setupCamera(cameraPreview.getWidth(), cameraPreview.getHeight());
+        }else{
+            cameraPreview.setSurfaceTextureListener(surfaceTextureListener);
+        }
+    }
+
+    @Override
+    protected void onPause(){
+        closeCamera();
+        stopBackgroundThread();
+        super.onPause();
+    }
+
+    private void setupCamera(int width, int height){
+        CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try{
+            for(String cameraId: cameraManager.getCameraIdList()){
+                CameraCharacteristics cameraCharacteristics = cameraManager.getCameraCharacteristics(cameraId);
+                if(cameraCharacteristics.get(CameraCharacteristics.LENS_FACING) == CameraCharacteristics.LENS_FACING_FRONT){
+                    continue;
+                }
+                cameraIds = cameraId;
+                return;
+            }
+        }catch(CameraAccessException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    private void closeCamera(){
+        if(cameraDevice != null){
+            cameraDevice.close();
+            cameraDevice = null;
+        }
+    }
+
+    private void startBackgroundThread(){
+        backgroundHandlerThread = new HandlerThread("ContextMonitoringApplication");
+        backgroundHandlerThread.start();
+        backgroundHandler = new Handler(backgroundHandlerThread.getLooper());
+    }
+
+    private void stopBackgroundThread(){
+        backgroundHandlerThread.quitSafely();
+        try{
+            backgroundHandlerThread.join();
+            backgroundHandlerThread = null;
+            backgroundHandler = null;
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        }
     }
 }
